fbce85ae382efc691fbd105a8624388a86227aac
Kostas Tsalikis <tsalikispk@gmail.com>
2024-11-04 13:41:17 +0200

    Fix store file path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("RELEASE_KEYSTORE_PATH"))
+            storeFile = file("keystore")
             storePassword = System.getenv("SIGNING_STORE_PASSWORD")
             keyAlias = System.getenv("SIGNING_KEY_ALIAS")
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
